7a791755348e9dea1ec5bd4e18b9fd899a4db05c
borobhai69 <45941112+borobhai69@users.noreply.github.com>
2018-12-17 08:19:36 -0500

    Add files via upload

---
+++ web.js
@@ -0,0 +1,48 @@
+
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display = 'block';
+}
+function revealMessage2() {
+	document.getElementById("hiddenMessage2").style.display = 'block';
+}
+function revealMessage3() {
+	document.getElementById("hiddenMessage3").style.display = 'block';
+}
+function revealMessage4() {
+	document.getElementById("hiddenMessage4").style.display = 'block';
+}
+function revealMessage5() {
+	document.getElementById("hiddenMessage5").style.display = 'block';
+}
+function revealMessage6() {
+	document.getElementById("hiddenMessage6").style.display = 'block';
+}
+function revealMessage7() {
+	document.getElementById("hiddenMessage7").style.display = 'block';
+}
+function revealMessage8() {
+	document.getElementById("hiddenMessage8").style.display = 'block';
+}
+function revealMessage9() {
+	document.getElementById("hiddenMessage9").style.display = 'block';
+}
+function revealMessage10() {
+	document.getElementById("hiddenMessage10").style.display = 'block';
+}
+function goBack() {
+	window.history.back();
+}
+function goForward() {
+	window.history.forward();
+}
+function bigImg(x) {
+    x.style.height = "400px";
+    x.style.width = "600px";
+    document.getElementById("caption").style.display = 'block';
+}
+
+function smallImg(x) {
+    x.style.height = "200px";
+    x.style.width = "300px";
+    document.getElementById("caption").style.display = 'none';
+}
